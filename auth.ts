94c87596b0b6a308bb2f35dd0ce08b8eb56e2280
Bekyo <bekyoverse@gmail.com>
2026-03-05 07:20:29 +0700

    debug: enable next-auth debug for production

--- auth.ts
+++ auth.ts
@@ -27,5 +27,5 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
     session: { strategy: "jwt" },
     secret: process.env.AUTH_SECRET,
-    debug: process.env.NODE_ENV === "development",
+    debug: true,
 });
